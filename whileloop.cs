3c9bf7a8387f216e5c19caf8a99125fd0ca57f00
rachel648 <106959706+rachel648@users.noreply.github.com>
2024-02-07 17:22:27 +0300

    Add files via upload

---
+++ whileloop.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string phrase = "God bless Kenya";
+        int count = 0;
+
+        while (count < 25)
+        {
+            Console.WriteLine(phrase);
+            count++;
+        }
+    }
+}
