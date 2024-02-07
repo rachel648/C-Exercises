b0d7af4721267f1cbc5de8296347ea580f759c53
rachel648 <106959706+rachel648@users.noreply.github.com>
2024-02-07 17:25:04 +0300

    Add files via upload

---
+++ dowhile.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string phrase = "God bless Kenya";
+        int count = 0;
+
+        do
+        {
+            Console.WriteLine(phrase);
+            count++;
+        } while (count < 25);
+    }
+}
---
+++ forloop.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string phrase = "God bless Kenya";
+
+        for (int i = 0; i < 25; i++)
+        {
+            Console.WriteLine(phrase);
+        }
+    }
+}
