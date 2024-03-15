74dced62a0a79c46be58673462b086d80f756165
rachel648 <106959706+rachel648@users.noreply.github.com>
2024-03-16 01:27:27 +0300

    Add files via upload

---
+++ CircleProgramTwo.cs
@@ -0,0 +1,54 @@
+using System;
+
+class Circle
+{
+   private double radius, area, circum;
+
+   public void input()
+   {
+      Console.Write("\nEnter the radius of the circle:> ");
+      radius = Convert.ToDouble(Console.ReadLine());
+   }
+
+   public void compute()
+   {
+     area = 22.0 / 7 * radius * radius;
+     circum = 22.0 / 7 * radius * 2;
+   }
+
+   public void output()
+   {
+      Console.WriteLine("The area of the rectangle is " + area + " and the circumference is " + circum);
+   }
+
+   public double Radius
+   {
+      get
+      {
+         return radius;
+      }
+      set
+      {
+         radius = value;
+      }
+   }
+}
+class CircleProgramTwo
+{
+   static void Main(string[] args)
+   {
+      Circle circ1 = new Circle();
+
+      Console.WriteLine("\nThe circle the first time:");
+      circ1.input();
+      circ1.compute();
+      circ1.output();
+
+      Console.WriteLine("\nThe radius is " + circ1.Radius);//Output of radius using properties
+
+      Console.WriteLine("\nThe circle the second time:");
+      circ1.Radius = 14;//setting the value of radius using properties
+      circ1.compute();
+      circ1.output();
+   }
+}
