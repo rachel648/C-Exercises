1c4f1229c80c821a95ca5e49af2eedd09ba1000b
rachel648 <106959706+rachel648@users.noreply.github.com>
2024-03-16 16:17:06 +0300

    Add files via upload

---
+++ marry.cs
@@ -0,0 +1,26 @@
+using System;
+
+  class Mario {
+    string love;
+
+    public void babe() {
+      Console.WriteLine("Mario & Ray");
+      
+    }
+
+    static void Main(string[] args) {
+
+      // create Dog object 
+      Mario marriage = new Mario();
+
+      // access breed of the Dog 
+      marriage.love = "lovebirds";
+      Console.WriteLine(marriage.love);
+
+      // access method of the Dog
+      marriage.babe();   
+
+      Console.ReadLine();
+     
+    }
+  }
