23bf443bf141dcdd204fcdc382b7c9d3f5aaa078
rachel648 <106959706+rachel648@users.noreply.github.com>
2024-05-07 21:55:46 +0300

    Add files via upload

--- degree.cs
+++ degree.cs
@@ -8,6 +8,7 @@ namespace CelsiusToFahrenheitConverter
         {
             Console.Write("Enter temperature in Celsius: ");
             double celsius = Convert.ToDouble(Console.ReadLine());
+
             double fahrenheit = ((celsius * 9) / 5) + 32;
             Console.WriteLine("The converted Fahrenheit temperature is: " + fahrenheit );
             Console.ReadLine();
