b23cbbdb09c476dc2127f526006b9aa468146e55
rachel648 <106959706+rachel648@users.noreply.github.com>
2024-05-10 01:48:08 +0300

    Add files via upload

--- five.cs
+++ five.cs
@@ -2,79 +2,24 @@ using System;
 
 class Program
 {
-  static void Main(string[] args)
-  {
-    Console.Write("Enter the number of items: ");
-    int numItems = int.Parse(Console.ReadLine());
-
-    string[] itemCodes = new string[numItems];
-    double[] prices = new double[numItems];
-    int[] quantities = new int[numItems];
-    double[] totalCosts = new double[numItems];
-    double[] discounts = new double[numItems];
-    double[] netCosts = new double[numItems];
-
-    double totalAmount = 0;
-
-    for (int i = 0; i < numItems; i++)
+    static void Main(string[] args)
     {
-      Console.Write($"\nEnter details for item {i + 1}:\n");
-      
-      // Get item details with input validation
-      bool validInput = false;
-      while (!validInput)
-      {
-        Console.Write("Product code: ");
-        itemCodes[i] = Console.ReadLine();
-        if (string.IsNullOrEmpty(itemCodes[i]))
+        // Prompting the user to enter a number
+        Console.WriteLine("Enter a number:");
+        double number = double.Parse(Console.ReadLine());
+
+        // Checking if the number is negative, positive, or zero
+        if (number < 0)
         {
-          Console.WriteLine("Item code cannot be null or empty. Please re-enter.");
+            Console.WriteLine("NEGATIVE");
+        }
+        else if (number > 0)
+        {
+            Console.WriteLine("POSITIVE");
         }
         else
         {
-          validInput = true;
+            Console.WriteLine("ZERO");
         }
-      }
-
-      Console.Write("Price: ");
-      prices[i] = double.Parse(Console.ReadLine());
-
-      Console.Write("Quantity: ");
-      quantities[i] = int.Parse(Console.ReadLine());
-
-      // Calculate costs for the item
-      totalCosts[i] = prices[i] * quantities[i];
-      CalculateDiscount(totalCosts[i], discounts, i);  // Call a separate method for discount calculation
-      netCosts[i] = totalCosts[i] - discounts[i];
-
-      totalAmount += netCosts[i];
     }
-
-    // Display the final table and total amount
-    Console.WriteLine("\n\nItem Code\tPrice\t\tQuantity\tTotal Cost\tDiscount\t\tNet");
-    Console.WriteLine("--------------------------------------------------------------------------");
-    for (int i = 0; i < numItems; i++)
-    {
-      Console.WriteLine($"{itemCodes[i],10} {prices[i],10:C2} {quantities[i],10} {totalCosts[i],15:C2} {discounts[i],15:C2} {netCosts[i],15:C2}");
-    }
-    Console.WriteLine("--------------------------------------------------------------------------");
-    Console.WriteLine("The total amount payable is " + totalAmount);
-  }
-
-  // Separate method for discount calculation
-  static void CalculateDiscount(double totalCost, double[] discounts, int index)
-  {
-    if (totalCost > 20000)
-    {
-      discounts[index] = totalCost * 0.14;
-    }
-    else if (totalCost >= 10000)
-    {
-      discounts[index] = totalCost * 0.1;
-    }
-    else
-    {
-      discounts[index] = 0;
-    }
-  }
 }
--- four.cs
+++ four.cs
@@ -1,121 +1,31 @@
 using System;
 
-class SupermarketItem
+public class NumberComputation
 {
-    private readonly string _itemCode; 
-    public double Price { get; set; }
-    public int Quantity { get; set; }
-    public double TotalCost { get; private set; }
-    public double Discount { get; private set; }
-    public double NetCost { get; private set; }
-
-    public SupermarketItem(string itemCode, double price, int quantity)
+    public static void Main(string[] args)
     {
-        if (string.IsNullOrEmpty(itemCode))
+        Console.WriteLine("Enter the first number:");
+        double number1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        double number2 = double.Parse(Console.ReadLine());
+
+        double result;
+
+        if (number1 > number2)
         {
-            throw new ArgumentException("Item code cannot be null or empty.");
+            result = number1 - number2;
+            Console.WriteLine("Result: " +result);
         }
-
-        _itemCode = itemCode;
-        Price = price;
-        Quantity = quantity;
-        CalculateTotalCost();
-        CalculateDiscount();
-        CalculateNetCost();
-    }
-
-    public SupermarketItem() { } 
-
-    ~SupermarketItem() 
-    {
-        Console.WriteLine($"Item {_itemCode} object disposed.");
-    }
-
-    private void CalculateTotalCost()
-    {
-        TotalCost = Price * Quantity;
-    }
-
-    private void CalculateDiscount()
-    {
-        if (TotalCost > 20000)
+        else if (number2 > number1)
         {
-            Discount = TotalCost * 0.14;
-        }
-        else if (TotalCost >= 10000)
-        {
-            Discount = TotalCost * 0.1;
+            result = number1 / number2;
+            Console.WriteLine("Result: "+result);
         }
         else
         {
-            Discount = 0;
+            result = number1 + number2;
+            Console.WriteLine($"Result: {result}"); // using string interpolatio
         }
-    }
-
-    private void CalculateNetCost()
-    {
-        NetCost = TotalCost - Discount;
-    }
-
-    public string GetItemCode()
-    {
-        return _itemCode;
-    }
-
-    public override string ToString() 
-    {
-        return GetItemCode().PadRight(10) + " " +
-       Price.ToString("C2").PadLeft(10) + " " +
-       Quantity.ToString().PadLeft(10) + " " +
-       TotalCost.ToString("C2").PadLeft(15) + " " +
-       Discount.ToString("C2").PadLeft(15) + " " +
-       NetCost.ToString("C2").PadLeft(15);
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.Write("Enter the number of items: ");
-        int numItems = int.Parse(Console.ReadLine());
-
-        SupermarketItem[] items = new SupermarketItem[numItems];
-
-        double totalAmount = 0;
-
-        for (int i = 0; i < numItems; i++)
-        {
-            Console.Write($"\nEnter details for item {i + 1}:\n");
-            Console.Write("Product code: ");
-            string itemCode = Console.ReadLine();
-            Console.Write("Price: ");
-            double price = double.Parse(Console.ReadLine());
-            Console.Write("Quantity: ");
-            int quantity = int.Parse(Console.ReadLine());
-
-            try
-            {
-                items[i] = new SupermarketItem(itemCode, price, quantity);
-                totalAmount += items[i].NetCost;
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(ex.Message);
-                i--; // Decrement i to allow user to re-enter details for this item
-            }
-        }
-
-        Console.WriteLine("\n\nItem Code\tPrice\t\tQuantity\tTotal Cost\tDiscount\t\tNet");
-        Console.WriteLine("--------------------------------------------------------------------------");
-        foreach (SupermarketItem item in items)
-        {
-            if (item != null) // Check for null items due to potential exceptions
-            {
-                Console.WriteLine(item);
-            }
-        }
-        Console.WriteLine("--------------------------------------------------------------------------");
-        Console.WriteLine("The total amount payable is " + totalAmount);
     }
 }
